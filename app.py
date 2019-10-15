632633feba909cfde75954fc3454ddbdd87db298
miftahihsan <30389126+miftahihsan@users.noreply.github.com>
2019-10-15 20:54:26 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-aws_access_key_id="ASIAVKUIDNMS4LMWXOAW"
-aws_secret_access_key="uvVkF4gW7k9d0Smdx/prGMleB7avvzOegMFgU23R"
-aws_session_token="FQoGZXIvYXdzEOz//////////wEaDCokCNQmJ3FyEHqiVCKUAwNbtNkSgFhkjkSFQUvE3A6F+KndjuVGiu+4i0oa1Y7ajMGd7jL7SLKb93OM0ejdqQn4RhTDKHNFBrX51VfFbUCKUuJmatCDhNTYBSJPq5J2pUCp/bJ5bPphicNrmDmRv+xn7JDBiHOoRp8+KsCwq5x0yxZjvGwpsN819eU02Olj/AqOaxAwfktUBxSuGXOsQ42XRu8OjgJOT+qH+KP7pHQ0CwK/RmA3MKjIzjg8aEVZ2opLmVPxa/XjHVo/LuVl/Xr+d4n3Xsvo+gFJpx+dtn1BUxe61EiQLH004Wx/5VnsPRuu4NWMWb0w+FXFfhKU+6u9afjqasqk+R/9osCaEDBFXjMskxkc3COygil8tTLFp/YwYwXHiyGCIQEa2VIuI5bebf4cY5ZrdKnArnxaOGry4p3n+FTXTjn4035NUuIhFSQtK35zAQ7wofXMfTxStBIy1mdcFlY5JiRj3fNPpmIUCAwFfomZe/96I16Qvmxx5KqkJIXU4Ki04WMLCvhy8E6LTjvtgdL3U58nPx8nN1lPC34WKPbyu+kF"
-
+aws_access_key_id=""
+aws_secret_access_key=""
+aws_session_token=""
 
 import boto3
 
